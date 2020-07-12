b440e611d47db1977c5f6455240fa978813c2d1f
Lukasz Kaczmarek <lukaszk76@gmail.com>
2020-07-12 13:57:02 +0200

    Frontend/Backend app body created (Vue/Spring/Hibernate)

---
+++ src/main/frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+	<h1>
+      {{welcome}}
+    </h1>
+  </div>
+</template>
+
+<script>
+    import "milligram";
+    import Vue from "vue";
+
+    export default {
+        data() {
+            return {
+                welcome: "Easy Survey by #CODE",
+            };
+        },
+    };
+</script>
+
+<style lang="scss">
+  #app {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+
+  .logo {
+    vertical-align: middle;
+  }
+
+  .alert {
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 2px solid black;
+    &-success {
+      background: lightgreen;
+      border-color: darken(lightgreen, 10%);
+    }
+    &-error {
+      background: indianred;
+      border-color: darken(indianred, 10%);
+      color: white;
+    }
+  }
+
+</style>
+
---
+++ src/test/java/easysurvey/EasySurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package easysurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
