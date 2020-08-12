a3cc1253268f19f2d739a2aea9626b9ccadc1db9
Lukasz Kaczmarek <lukaszk76@gmail.com>
2020-08-12 19:28:14 +0200

    rest service for /api/survey and api/survey/ID GET readt

--- src/main/java/easysurvey/EasySurveyApplication.java
+++ src/main/java/easysurvey/EasySurveyApplication.java
@@ -5,16 +5,17 @@ import easysurvey.persistence.HibernateUtil;
 import easysurvey.persistence.SurveyService;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EasySurveyApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(EasySurveyApplication.class, args);
-		SurveyService surveyService = new SurveyService();
-		surveyService.run();
-		surveyService.close();
+		SpringApplication.run(EasySurveyApplication.class, args);
+//		SurveyService surveyService = new SurveyService();
+//		surveyService.run();
+//		surveyService.close();
 	}
 }
 
--- src/main/java/easysurvey/dataModel/controllers/SurveyRestController.java
+++ src/main/java/easysurvey/dataModel/controllers/SurveyRestController.java
@@ -1,9 +1,17 @@
 package easysurvey.dataModel.controllers;
 
 
+import easysurvey.dataModel.Survey;
 import easysurvey.persistence.SurveyService;
+
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +21,20 @@ public class SurveyRestController {
     @Autowired
     SurveyService surveyService;
 
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> getSurveys() {
+
+        Collection<Survey> surveys = surveyService.getAllSurveys();
+        return new ResponseEntity<Collection<Survey>>(surveys, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getSurvey(@PathVariable("id") long surveyID) {
+	    Survey survey = surveyService.getSurvey(surveyID);
+		if (survey == null) { 
+		return new ResponseEntity(HttpStatus.NOT_FOUND);
+		} 	
+		return new ResponseEntity<Survey>(survey, HttpStatus.OK); 
+	}
 
 }
