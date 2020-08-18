ce5fc9763e3d8d610efc8613219f22d394063a5a
Lukasz Kaczmarek <lukaszk76@gmail.com>
2020-08-18 17:11:31 +0200

    FillSurveyQuestion component displays questions and potential answers.

--- src/main/frontend/src/views/FillSurvey.vue
+++ src/main/frontend/src/views/FillSurvey.vue
@@ -1,15 +1,24 @@
 <template>
+
+  <transition
+    name="custom-classes-transition"
+    enter-active-class="animated bounceInRight"
+    leave-active-class="animated bounceOutRight"
+  >
   <div v-if="survey" id="fill_survey">
-    <h4> {{survey.description}}</h4>
+    <br>
+    <h3> {{survey.description}}</h3>
     <div v-for="question in survey.questions" :key="question.id">
-      {{question.id}}.{{question.questionText}}
-      <p v-for="potentialanswer in question.potentialQuestionAnswers" :key="potentialanswer.id">{{potentialanswer.text}}</p>
+      <fill-survey-question v-bind:question="question" @error="failure($event)" @success="success($event)"></fill-survey-question>
     </div>
   </div>
+  </transition>
 </template>
 
 <script>
+    import FillSurveyQuestion from "./FillSurveyQuestion";
     export default {
+      components: {FillSurveyQuestion},
       props: ["surveyId"],
       
       data() {
@@ -37,17 +46,6 @@
           })
         },
 
-        // loadQuestions(){
-        //   this.$http.get('survey/' + this.surveyId + 'questions')
-        //   .then( response => {
-        //     this.survey.questions = response.body;
-        //     this.success("successfuly loaded survey ID" + this.survey.id + " with " + this.survey.questions.length + " questions");
-
-        //   })
-        //   .catch( response => {
-        //     this.failure('Error ' + response.status + ' while loading questions to the survey.');
-        //   })
-        // },
 
         success(message) {
           this.$emit("success", message);
@@ -65,3 +63,15 @@
 
     };
 </script>
+
+<style>
+
+	.fade-enter-active, .fade-leave-active {
+	transition: opacity .5s;
+	}
+
+	.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+	opacity: 0;
+	}
+
+</style>
---
+++ src/main/frontend/src/views/FillSurveyQuestion.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<br>
+		<h5>{{question.questionText}}</h5>
+		<div id="potential_answers" v-for="potentialQuestionAnswer in question.potentialQuestionAnswers" :key="potentialQuestionAnswer.id">
+			<input type="radio" :id="potentialQuestionAnswer.id" :value="potentialQuestionAnswer.id" :name="question.id" v-model="picked">
+			{{potentialQuestionAnswer.text}}
+		</div>
+	</div>
+	
+</template>
+
+<script>
+
+	export default {
+      props: ["question"],
+      
+      data() {
+          return {
+			  message: "",
+			  picked: [],
+          }
+	  },
+
+	  methods: {
+		success(message) {
+          this.$emit("success", message);
+        },
+
+        failure(message) {
+          this.$emit("error", message);
+        },  
+	  },
+	  mounted(){
+		  alert(question.text)
+	  }
+	}
+
+</script>
