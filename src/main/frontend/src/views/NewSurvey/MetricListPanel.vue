ae6ff5716238a920da9c5d607647a5fe2c9476fc
Marcin <mieker@wp.pl>
2020-09-10 21:42:38 +0200

    deleting metricQuestions from MetricListPanel works

--- src/main/frontend/src/views/NewSurvey/MetricListPanel.vue
+++ src/main/frontend/src/views/NewSurvey/MetricListPanel.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="metricList" v-if="this.questions.length > 0">
     <ul v-for="question in this.questions" v-bind:key="question">
-        <button class="button-small" style="margin: 0 10px">X</button> {{ questions.indexOf(question)+1 }}. {{ question.questionText }} 
+        <button class="button-small" style="margin: 0 10px" @click="questions.splice(questions.indexOf(question), 1)">X</button> {{ questions.indexOf(question)+1 }}. {{ question.questionText }} 
         <li v-for="answer in question.answers" v-bind:key="answer">
             - {{ answer }}
         </li>
