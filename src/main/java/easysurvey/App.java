cddf02a4dbf2e1892fdfa41b692d0b4403ad256d
Marcin <mieker@wp.pl>
2020-09-17 21:14:47 +0200

    db changed from sqlite to heroku/postgres

--- src/main/java/easysurvey/App.java
+++ src/main/java/easysurvey/App.java
@@ -9,11 +9,6 @@ public class App {
 
 	public static void main(String[] args) {
 		SurveyService surveyService = new SurveyService();
-		surveyService.createSurvey();
-//		System.out.println(surveyService.statCalculation());
-//		System.out.println(surveyService.statAnswers(1,1));
-//		System.out.println(surveyService.statAnswers(1,2));
-//		System.out.println(surveyService.statAnswers(1,3));
 		surveyService.close();
 		SpringApplication.run(App.class, args);
 	}
