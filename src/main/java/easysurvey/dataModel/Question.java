c744fce62ce0527444e59bb759c1338f50dfc23c
Marcin <mieker@wp.pl>
2020-10-03 13:17:36 +0200

    questionAnswers deleted from Metric, metricAnswers deleted from Questions = postresql bug fixed

--- src/main/java/easysurvey/dataModel/Question.java
+++ src/main/java/easysurvey/dataModel/Question.java
@@ -22,10 +22,6 @@ public class Question implements Comparable<Question>
     @OneToMany(mappedBy = "question",cascade = CascadeType.PERSIST)
     private Set<QuestionAnswer> questionAnswers = new HashSet<>();
 
-    @OneToMany(mappedBy = "metric",cascade = CascadeType.PERSIST)
-    private Set<MetricAnswer> metricAnswers = new HashSet<>();
-
-
     public Question() {
     }
 
